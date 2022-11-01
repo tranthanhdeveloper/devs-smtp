3ed06630167410daa7419e76860e6d7a905ad0dd
Thanh TranV <thanh.tranv@sai-digital.com>
2022-11-01 13:26:58 +0700

    Add download email feature

--- src/main/java/com/tvtsoftware/devssmtp/controller/DevSmtpController.java
+++ src/main/java/com/tvtsoftware/devssmtp/controller/DevSmtpController.java
@@ -2,13 +2,26 @@ package com.tvtsoftware.devssmtp.controller;
 
 import com.tvtsoftware.devssmtp.model.Email;
 import com.tvtsoftware.devssmtp.services.EmailService;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.InputStreamResource;
 import org.springframework.data.domain.Page;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 @Controller
 public class DevSmtpController {
@@ -43,5 +56,20 @@ public class DevSmtpController {
     public String deleteAllEmail() {
         emailService.deleteAllEmail();
         return "redirect:/";
+    }
+
+
+    @GetMapping(value = "/email/download/{id}")
+    @ResponseBody
+    public ResponseEntity downloadEmail(@PathVariable("id") Long id) throws IOException {
+        Email email = emailService.getEmailByUID(id);
+        byte[] bytes = email.getRaw().getBytes(StandardCharsets.UTF_8);
+        ByteArrayResource resource = new ByteArrayResource(bytes);
+        String filename = StringUtils.trim(email.getSubject()) + ".eml";
+        return ResponseEntity.ok()
+                .contentLength(bytes.length)
+                .header("Content-type", "application/octet-stream")
+                .header("Content-disposition", "attachment; filename=" + URLEncoder.encode(filename, StandardCharsets.UTF_8))
+                .body(resource);
     }
 }
