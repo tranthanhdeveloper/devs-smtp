d5090d16ecb67ecdde85cc1c1e74ed139ab1689b
Thanh TranV <thanh.tranv@sai-digital.com>
2022-11-02 14:56:21 +0700

    Update UI

--- src/main/java/com/tvtsoftware/devssmtp/listener/EmailReceivedEventListener.java
+++ src/main/java/com/tvtsoftware/devssmtp/listener/EmailReceivedEventListener.java
@@ -3,6 +3,7 @@ package com.tvtsoftware.devssmtp.listener;
 import com.tvtsoftware.devssmtp.handler.MultipartHandler;
 import com.tvtsoftware.devssmtp.model.Email;
 import com.tvtsoftware.devssmtp.repository.EmailRepository;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.EventListener;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
@@ -10,7 +11,7 @@ import org.springframework.stereotype.Component;
 import javax.transaction.Transactional;
 
 @Component
-public class EmailReceivedEventListener {
+public class EmailReceivedEventListener{
 
     private EmailRepository emailRepository;
     private MultipartHandler multipartHandler;
