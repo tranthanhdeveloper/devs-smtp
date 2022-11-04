4a9829640f9daf126fc880a540ad0157fc3a98b7
Thanh TranV <thanh.tranv@sai-digital.com>
2022-11-04 15:04:48 +0700

    Rename file

--- src/main/java/com/tvtsoftware/devssmtp/handler/EmailHandler.java
+++ src/main/java/com/tvtsoftware/devssmtp/handler/EmailHandler.java
@@ -3,6 +3,6 @@ package com.tvtsoftware.devssmtp.handler;
 import com.tvtsoftware.devssmtp.model.Email;
 import org.apache.james.protocols.smtp.MailEnvelope;
 
-public interface MultipartHandler {
+public interface EmailHandler {
     Email processEmail(MailEnvelope mailEnvelope);
 }
